01780ca541ed382ca600539043976664121dd49a
Brian Babey <brian.babey@abrigo.com>
2022-12-02 08:03:37 -0500

    create a working minimal vue app

---
+++ src/components/HelloLibrary.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+    msg: String,
+});
+</script>
+
+<template>
+    <div>
+        <h2>{{ msg }}</h2>
+        <p>I am a minimal component in LibraryApp.</p>
+    </div>
+</template>
